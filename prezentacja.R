88606c20e2131a3575f7361fb16dd6a9442d5f7f
norbertryciak <ryciakn@student.mini.pw.edu.pl>
2014-06-01 19:24:24 +0200

    Prezentacja plus przyklad

--- prezentacja.R
+++ prezentacja.R
@@ -120,6 +120,49 @@ dissimilarity(crude[[1]], crude[[2]], method = "eJaccard")
 #######   PRZYKLAD    ######
 
 
+wiki <- Corpus(VectorSource(c(
+  stri_flatten(readLines("http://en.wikipedia.org/wiki/Integral"),col=" "),
+  stri_flatten(readLines("http://en.wikipedia.org/wiki/Riemann_integral"),col=" "),
+  stri_flatten(readLines("http://en.wikipedia.org/wiki/Riemann%E2%80%93Stieltjes_integral"),col=" "),
+  stri_flatten(readLines("http://en.wikipedia.org/wiki/Derivative"),col=" "),
+  stri_flatten(readLines("http://en.wikipedia.org/wiki/Limit_of_a_sequence"),col=" "),
+  stri_flatten(readLines("http://en.wikipedia.org/wiki/Monoid"),col=" "),
+  stri_flatten(readLines("http://en.wikipedia.org/wiki/Group_%28mathematics%29"),col=" "),
+  stri_flatten(readLines("http://en.wikipedia.org/wiki/Ring_%28mathematics%29"),col=" "))))
+
+inspect(wiki) #wyglada paskudnie...
+
+wiki2 <- tm_map(wiki,function(x) stri_replace_all_regex(x,"<.+?>"," "))
+wiki3 <- tm_map(wiki2,function(x) stri_replace_all_fixed(x,"\t"," "))
+
+wiki4 <- tm_map(wiki3, PlainTextDocument)
+wiki5 <- tm_map(wiki4, stripWhitespace)
+wiki5 <- tm_map(wiki5, tolower)
+wiki5 <- tm_map(wiki5, removeWords, stopwords("english"))
+wiki5 <- tm_map(wiki5, removePunctuation)
+
+
+
+inspect(wiki5)  #wyglada niezle
+wikiTDM <- TermDocumentMatrix(wiki5)
+wikiTDM
+
+dim(wikiTDM)
+inspect(wikiTDM[stri_detect_regex(rownames(wikiTDM),"^(integr)"),])
+
+inspect(wikiTDM[c("integral","derivative","riemann","limit","infinity","operation"),])
+wikidissim <- dissimilarity(wikiTDM, method = "cosine") #zwraca obiekt klasy 'dist'!
+
+wikidissim2 <- as.matrix(wikidissim)
+rownames(wikidissim2) <- c("Int.","Riem. int.","R. S. int.",
+                           "Der.","Lim.","Mon.","Group","Ring") 
+colnames(wikidissim2) <- c("Integral","Riem. integral","R. Stieltjes int.",
+                           "Derivative","Limit...","Monoid","Group","Ring")
+wikidissim2
+h <- hclust(wikidissim, method="ward")
+plot(h,labels=c("Integral","Riemann integral","Riemann and Stieltjes integral",
+                "Derivative","Limit of a sequence","Monoid","Group","Ring"))
+
 
 
 
